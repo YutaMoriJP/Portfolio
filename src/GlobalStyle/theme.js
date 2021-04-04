9e84ac601d6f3f64175a995e02cb3b5fc1410558
Yuta Mori <5569823+YutaMoriJP@users.noreply.github.com>
2021-04-04 15:09:10 -0700

    added tictactoe app

--- src/GlobalStyle/theme.js
+++ src/GlobalStyle/theme.js
@@ -4,7 +4,7 @@ export const styledTheme = {
     color: "#f8f9fa",
     black: "#212529",
     buttonLink: "#212529",
-    titleColor: "#fff0f6",
+    titleColor: "#e5dbff",
     titleGradient: {
       background: "#fff0f6",
       webkit: "-webkit-linear-gradient(to right, #fff0f6 0%, #d6336c 100%)",
@@ -25,10 +25,9 @@ export const styledTheme = {
       linear: "linear-gradient(to right,#121FCF 0%, #CF1512 100%)",
     },
   },
-}; //#748ffc
+};
+
 export const materialColors = {
-  primary: { dark: "#fff0f6", light: "#1D428A" },
+  primary: { dark: "#e5dbff", light: "#1D428A" },
   secondary: { dark: "#f783ac", light: "#d6336c" },
 };
-//AFDBF5
-//d0ebff
--- src/data/project.js
+++ src/data/project.js
@@ -1,6 +1,6 @@
 import nytt from "../Images/nyt.png";
 import todoo from "../Images/note.png";
-
+import tictactoe from "../Images/tictactoe.png";
 import { v4 as uuidv4 } from "uuid";
 
 const projects = [
@@ -9,7 +9,8 @@ const projects = [
     img: nytt,
     id: uuidv4(),
     url: "https://xenodochial-mcclintock-d84f7d.netlify.app/",
-    description: "A project utilizing the NYT API to read condensed articles.",
+    description:
+      "A project utilizing the NYT API to read condensed articles. The user can either type in a keyword or simply click on the suggested tag like 'news' and make a request that way. If no matching article is found, a message notifying the user is rendered instead.",
     tags: [
       { tag: "JavaScript", id: uuidv4() },
       { tag: "HTML5", id: uuidv4() },
@@ -18,6 +19,7 @@ const projects = [
       { tag: "React Router", id: uuidv4() },
       { tag: "Styled Component", id: uuidv4() },
       { tag: "Fetch API", id: uuidv4() },
+      { tag: "Material UI", id: uuidv4() },
     ],
   },
   {
@@ -25,13 +27,31 @@ const projects = [
     img: todoo,
     id: uuidv4(),
     url: "https://wonderful-euclid-5863c6.netlify.app/",
-    description: "A todo app that allows users to create a todo list.",
+    description:
+      "A todo app that allows users to create a todo list and either mark the post as completed or remove it. Users also have the option to change the background color of the post as well as sort their posts where the new posts appear at the top or bottom.",
     tags: [
       { tag: "JavaScript", id: uuidv4() },
       { tag: "HTML5", id: uuidv4() },
       { tag: "CSS3", id: uuidv4() },
       { tag: "React JS", id: uuidv4() },
       { tag: "Styled Component", id: uuidv4() },
+      { tag: "Material UI", id: uuidv4() },
+    ],
+  },
+  {
+    name: "Tic-Tac-Toe Game",
+    img: tictactoe,
+    id: uuidv4(),
+    url: "https://unruffled-wright-087758.netlify.app/",
+    description:
+      "A simple Tic Tac Toe game. 2 players can play the game where one player is represented with 'X' and the other is represented with 'O'. There are a possible 8 winning combination and once that winning combination is met by player O or X, the game is finished.",
+    tags: [
+      { tag: "JavaScript", id: uuidv4() },
+      { tag: "HTML5", id: uuidv4() },
+      { tag: "CSS3", id: uuidv4() },
+      { tag: "React JS", id: uuidv4() },
+      { tag: "Styled Component", id: uuidv4() },
+      { tag: "React Confetti", id: uuidv4() },
     ],
   },
 ];
