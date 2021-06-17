400dc99731427c9a4ccd2f8160b7ce2c6eda826c
Yuta Mori <5569823+YutaMoriJP@users.noreply.github.com>
2021-06-17 08:11:33 -0700

    added lazy loading for images

--- src/Components/Image/Image.js
+++ src/Components/Image/Image.js
@@ -1,6 +1,38 @@
 import React from "react";
 import placeholder from "../../Images/notfound.jpeg";
 import styled from "styled-components";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
+const ImageStyled = styled(LazyLoadImage)`
+  width: ${props => props.width || "300px"};
+  height: ${props => props.height || "200px"};
+`;
+
+const Image = ({
+  src = placeholder,
+  alt = "not found image",
+  width,
+  height,
+}) => {
+  const handleError = event => {
+    event.target.src = placeholder;
+  };
+  const handleSuccess = event => {
+    console.log(event);
+    event.target.src = placeholder;
+  };
+  return (
+    <ImageStyled
+      src={src}
+      alt={alt}
+      width={width}
+      height={height}
+      onError={handleError}
+      onSuccess={handleSuccess}
+    />
+  );
+};
+/*
 
 const ImageStyled = styled.img`
   width: ${props => props.width || "300px"};
@@ -16,4 +48,5 @@ const Image = ({
   return <ImageStyled src={src} alt={alt} width={width} height={height} />;
 };
 
+*/
 export default Image;
